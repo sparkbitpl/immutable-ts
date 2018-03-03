4577fe4206a8509b685029d728b561c1286387bc
Jedrek Fulara <jedrek@sparkbit.pl>
2018-03-03 14:59:01 +0100

    allow both immutable and mutable elements in setters

--- src/immutable-typescript.ts
+++ src/immutable-typescript.ts
@@ -5,14 +5,14 @@ export type Immutable<T> = {
 export interface ImmutableArray<T> extends Array<T> {}
 
 export class ImmutableUtils {
-    public static setValue<T,K extends keyof T>(obj: Immutable<T>, key: K, val: T[K]): Immutable<T> {
+    public static setValue<T,K extends keyof T>(obj: Immutable<T>, key: K, val: T[K] | Immutable<T[K]>): Immutable<T> {
         const copy = new (obj.constructor as { new (): Immutable<T> })();
         (<any>Object).assign(copy, obj);
         (<any>copy)[key] = val;
         return copy;
     }
 
-    public static setValue2<T,K extends keyof T, K2 extends keyof T[K]>(obj: Immutable<T>, key: K, key2: K2, val: T[K][K2]): Immutable<T> {
+    public static setValue2<T,K extends keyof T, K2 extends keyof T[K]>(obj: Immutable<T>, key: K, key2: K2, val: T[K][K2] | Immutable<T[K][K2]>): Immutable<T> {
         const copy = new (obj.constructor as { new (): Immutable<T> })();
         (<any>Object).assign(copy, obj);
         (<any>copy)[key] = ImmutableUtils.setValue(obj[key], key2, val);
@@ -20,7 +20,7 @@ export class ImmutableUtils {
     }
 
     public static setValue3<T,K extends keyof T, K2 extends keyof T[K], K3 extends keyof T[K][K2]>
-            (obj: Immutable<T>, key: K, key2: K2, key3:K3, val: T[K][K2][K3]): Immutable<T> {
+            (obj: Immutable<T>, key: K, key2: K2, key3:K3, val: T[K][K2][K3] | Immutable<T[K][K2][K3]>): Immutable<T> {
         const copy = new (obj.constructor as { new (): Immutable<T> })();
         (<any>Object).assign(copy, obj);
         (<any>copy)[key] = ImmutableUtils.setValue2(obj[key], key2, key3, val);
@@ -28,21 +28,21 @@ export class ImmutableUtils {
     }
 
     public static setValue4<T,K extends keyof T, K2 extends keyof T[K], K3 extends keyof T[K][K2], K4 extends keyof T[K][K2][K3]>
-            (obj: Immutable<T>, key: K, key2: K2, key3: K3, key4: K4, val: T[K][K2][K3][K4]): Immutable<T> {
+            (obj: Immutable<T>, key: K, key2: K2, key3: K3, key4: K4, val: T[K][K2][K3][K4] | Immutable<T[K][K2][K3][K4]>): Immutable<T> {
         const copy = new (obj.constructor as { new (): Immutable<T> })();
         (<any>Object).assign(copy, obj);
         (<any>copy)[key] = ImmutableUtils.setValue3(obj[key], key2, key3, key4, val);
         return copy;
     }
 
-    public static unshift<T>(arr: Immutable<Array<T>>, ...elements: T[]): Immutable<Array<T>> {
-        const copy = [...(arr as Array<T>)];
+    public static unshift<T>(arr: Immutable<Array<T>>, ...elements: (T | Immutable<T>)[]): Immutable<Array<T>> {
+        const copy = [...(arr as Array<any>)];
         copy.unshift(...elements);
         return copy;
     }
 
-    public static setIndex<T>(arr: Immutable<Array<T>>, index: number, value: T): Immutable<Array<T>> {
-        const copy = [...(arr as Array<T>)];
+    public static setIndex<T>(arr: Immutable<Array<T>>, index: number, value: T | Immutable<T>): Immutable<Array<T>> {
+        const copy = [...(arr as Array<any>)];
         copy[index] = value;
         return copy;
     }
